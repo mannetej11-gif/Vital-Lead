ca186a92227f9621d3c300a5d81c95cec46105bf
Sai teja <mannetej11@gmail.com>
2026-08-05 03:07:46 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// If you deploy this to GitHub Pages under a repo name (e.g.
-// https://yourname.github.io/vitallead/), uncomment and set base below.
-export default defineConfig({
-  plugins: [react()],
-  // base: "/vitallead/",
-});
